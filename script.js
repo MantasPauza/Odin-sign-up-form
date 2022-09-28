2c562af513373b84d25b1c55ab93066ad5c0db73
Mantas <mantaspauza@gmail.com>
2022-09-28 18:55:05 +0300

    fix: :bug: fix password check

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// Time to script!
+const submitButton = document.querySelector('#submit_button');
+
+submitButton.addEventListener('click', (e) => {
+  const password = document.querySelector('#password').value;
+  const confirmPassword = document.querySelector('#confirm_password').value;
+
+  if (!password || !confirmPassword) {
+    return;
+  }
+
+  if (password !== confirmPassword) {
+    alert('Password and confirmation do not match.');
+    e.preventDefault();
+  }
+});
